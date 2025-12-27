77e8fb246c7471a1beaf9431984c6335435ba129
prathameshghod <prathameshghodmare10@gmail.com>
2025-12-27 15:03:02 +0530

    Initial commit

--- file/love.js
+++ file/love.js
@@ -168,8 +168,8 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("Click Me:) ", 30, -5);
-            ctx.fillText("Birthday Queen !", 28, 10);
+            ctx.fillText("Idhar daba :) ", 30, -5);
+            ctx.fillText("Ghabra mat !", 28, 10);
             ctx.restore();
         },
         clear: function() {
@@ -701,8 +701,8 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("Click Me:) ", 30, -5);
-            ctx.fillText("Birthday Queen !", 28, 10);
+            ctx.fillText("Idhar daba :) ", 30, -5);
+            ctx.fillText("Ghabra mat !", 28, 10);
             ctx.restore();
         },
         clear: function() {
